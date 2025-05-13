2e315ca83626dfd2a32e758586e2c5265c311236
RadioNoiseE <j18516785606@icloud.com>
2025-05-13 12:22:25 +0800

    return in all branches

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -152,4 +152,5 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
         if (!process_socd_cleaner(keycode, record, &socd_v)) return false;
         if (!process_socd_cleaner(keycode, record, &socd_h)) return false;
     }
+    return true;
 }
