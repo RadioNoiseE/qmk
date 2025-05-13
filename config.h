5d0df1c6fef57ac486f315b67fabe1cd112c9df6
RadioNoiseE <j18516785606@icloud.com>
2025-05-13 08:10:22 +0800

    guard nprint and ndebug

--- config.h
+++ config.h
@@ -178,10 +178,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /* disable debug print */
-#define NO_DEBUG
+#ifndef NO_DEBUG
+#    define NO_DEBUG
+#endif
 
 /* disable print */
-#define NO_PRINT
+#ifndef NO_PRINT
+#    define NO_PRINT
+#endif
 
 /* disable action features */
 // #define NO_ACTION_LAYER
