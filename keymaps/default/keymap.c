83d875fa4e3655f0d46df587982fe9b6e16ff31b
RadioNoiseE <j18516785606@icloud.com>
2025-05-13 15:18:35 +0800

    flip by xor with true

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -165,7 +165,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
 
     switch (keycode) {
         case _SOCD:
-            if (record->event.pressed) socd_cleaner_enabled ^= socd_cleaner_enabled;
+            if (record->event.pressed) socd_cleaner_enabled ^= true;
             break;
         case _DYMC:
             if (record->event.pressed) {
