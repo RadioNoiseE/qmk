160a030fea724ed56967ec3f3299dd06e2d9058d
RadioNoiseE <j18516785606@icloud.com>
2025-05-15 12:43:44 +0800

    remove redundant key mapping

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -141,7 +141,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TRNS, _DYMC,   _DYMC,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,
             KC_TRNS, KC_TRNS, KC_TRNS, EE_CLR,  QK_BOOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,
             KC_TRNS, KC_TRNS, _SOCD,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, QK_LOCK, KC_TRNS, KC_TRNS,          KC_TRNS,
-            KC_TRNS,          KC_TRNS, KC_TRNS, _DYMC,   KC_TRNS, QK_RBT,  NK_TOGG, DM_RSTP, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,             KC_TRNS,
+            KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, QK_RBT,  NK_TOGG, _DYMC,   KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,             KC_TRNS,
             KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS
     ),
 };
